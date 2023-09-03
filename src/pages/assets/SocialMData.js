afe120c8931946084b92a0911a84c93b9be569a0
amank5788 <88306289+amank5788@users.noreply.github.com>
2023-09-03 13:07:00 +0530

    all at once

---
+++ src/pages/assets/SocialMData.js
@@ -0,0 +1,27 @@
+const social_media_data = [
+  {
+    index: 1,
+    icon_class: "fas fa fa-facebook-square",
+    where_to_go_link:
+      "https://www.facebook.com/aman.13088.kumar?mibextid=ZbWKwL",
+  },
+  {
+    index: 2,
+    icon_class: "fas fa fa-github",
+    where_to_go_link: "https://github.com/amank5788",
+  },
+  {
+    index: 3,
+    icon_class: "fas fa fa-instagram",
+    where_to_go_link: "https://instagram.com/aman8517?utm_source=qr&igshid=MThlNWY1MzQwNA==",
+  },
+  {
+    index: 4,
+    icon_class: "fas fa fa-linkedin",
+    where_to_go_link: "https://www.linkedin.com/in/aman-kumar-03841b1b7",
+  },
+ 
+  
+];
+
+export default social_media_data;
