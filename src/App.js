4a05bd2ab8b1426ed5e258e7ffdaf2f4fda2cc4e
amank5788 <88306289+amank5788@users.noreply.github.com>
2023-12-22 22:31:20 +0530

    commit last

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Frontpage from './pages/Frontpage';
 import About from './pages/About';
-import Contact from './pages/Contact';
 import EducationCert from './pages/EducationCert';
 import ProjectExp from './pages/Project_exp';
 import Skills from './pages/Skills';
 import Navbar from './pages/Navbar';
 import Footer from './pages/Footer';
-import Login from './pages/components/Login';
-import AdminUser from './pages/components/AdminUser';
+
 
 const App = () => {
   return (
@@ -24,7 +22,7 @@ const App = () => {
               <Frontpage />
               <About />
               <ProjectExp />
-              <Contact />
+          
               <Footer />
             </>
           }
@@ -48,8 +46,7 @@ const App = () => {
             </>
           }
         />
-        <Route exact path="/admin/login" element={<Login />} />
-        <Route exact path="/user-backend" element={<AdminUser />} />
+
       </Routes>
     </>
   );
